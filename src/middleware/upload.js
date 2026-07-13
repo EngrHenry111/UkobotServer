c4be5e33e085fe103f13b8586587c0eabbf60f05
EngrHenry111 <henryengrakpan@gmail.com>
2026-07-13 22:52:37 +0100

    upload deb

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,37 +1,19 @@
-// import multer from "multer";
-// import { CloudinaryStorage } from "multer-storage-cloudinary";
-// import cloudinary from "../config/cloudinary.js";
-
-// const storage = new CloudinaryStorage({
-//   cloudinary,
-//   params: async (req, file) => ({
-//     folder: "chairman_portfolio",
-//     resource_type: "image",
-//     allowed_formats: ["jpg", "jpeg", "png", "webp"],
-//   }),
-// });
-
-// export default multer({ storage });
-
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
 
-console.log("Cloudinary Config:");
-console.log(cloudinary.config());
-
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req, file) => ({
     folder: "chairman_portfolio",
     resource_type: "image",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
-  },
+  }),
 });
 
-const upload = multer({ storage });
+export default multer({ storage });
 
-export default upload;
+
 // import multer from "multer";
 // import { CloudinaryStorage } from "multer-storage-cloudinary";
 // import cloudinary from "../config/cloudinary.js";
